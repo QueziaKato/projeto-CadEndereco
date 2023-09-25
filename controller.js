48147134ff4add8437bb16932158f2e2a3ffe802
Quezia <quezia.kato@alunoescolassociais.org.br>
2023-09-25 11:57:16 -0300

    limpar formulário

---
+++ controller.js
@@ -0,0 +1,10 @@
+'use restrict'; //Modo restrito
+
+//Limpar formulário
+const limparFormulario =(endereco) =>{
+    document.getElementById('rua').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+
+}
